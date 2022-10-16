0a8bd101ec1bfd5a643f8023132bc161e83b788e
ashraf-a-khan <khan.ashraf2311@gmail.com>
2022-10-15 21:45:15 -0400

    Adds client through modal

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,11 +4,12 @@ export default function Header() {
   return (
     <nav className='navbar bg-light mb-4 p-0'>
       <div className='container'>
-        <a className='navbar-brand' href='/' />
-        <div className='d-flex'>
-          <img src={logo} alt='logo' className='mr-2' />
-          <div>ProjectMgmt</div>
-        </div>
+        <a className='navbar-brand' href='/'>
+          <div className='d-flex'>
+            <img src={logo} alt='logo' className='mr-2' />
+            <div>ProjectMgmt</div>
+          </div>
+        </a>
       </div>
     </nav>
   );
